ad733af89eecadb791381310f4c205cfae7af3f8
Jelani Woods <jelani@firstdraft.com>
2021-05-14 22:51:31 -0500

    Edit/Update todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,25 @@
 class TodosController < ApplicationController
   def index
   end
+
   def all_todos
     completed = Todo.where(completed: true)
     pending = Todo.where(completed: false)
     render json: { completed: completed, pending: pending }
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update!(todo_params)
+      render json: { message: "Todo Item updated successfully" }
+    else
+      render json: { message: "An error occured" }
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:id, :title, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "todos#index"
   get 'todos/index'
   get "todos/all_todos"
+  put "todos/update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
