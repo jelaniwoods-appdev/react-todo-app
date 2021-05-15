478aecda7e4285d4c681e114f6517a4ea28adaa6
Jelani Woods <jelani@firstdraft.com>
2021-05-14 22:22:45 -0500

    Add components

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,9 @@
 class TodosController < ApplicationController
   def index
   end
+  def all_todos
+    completed = Todo.where(completed: true)
+    uncompleted = Todo.where(completed: false)
+    render json: { completed: completed, uncompleted: uncompleted }
+  end
 end
