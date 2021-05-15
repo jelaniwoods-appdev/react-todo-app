4d44f3ebda39f238259f699f54ddc29a44255adf
Jelani Woods <jelani@firstdraft.com>
2021-05-14 22:07:47 -0500

    Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_232328) do
+ActiveRecord::Schema.define(version: 2021_05_15_030538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
